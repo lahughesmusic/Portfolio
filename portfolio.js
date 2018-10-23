28dca93c952456b8c29c2fb1c2baa9297c5d47ec
Lea Hughes <lahughesmusic@gmail.com>
2018-10-23 12:31:54 -0500

    add files

--- portfolio.js
+++ portfolio.js
@@ -1,111 +1,17 @@
 function pineHover(element) {
-    element.setAttribute('src', './assets/project/opening_hover.png');
+    element.setAttribute('src', './assets/PROJECT/opening_hover.png');
   }
   
   function pineUnhover(element) {
-    element.setAttribute('src', './assets/project/opening.png');
+    element.setAttribute('src', './assets/PROJECT/opening.png');
   }
 
   function jungleHover(element) {
-    element.setAttribute('src', './assets/project/junglebook_hover.png');
+    element.setAttribute('src', './assets/PROJECT/junglebook_hover.png');
   }
   
   function jungleUnhover(element) {
-    element.setAttribute('src', './assets/project/junglebook.png');
+    element.setAttribute('src', './assets/PROJECT/junglebook.png');
   }
 
 
-
-
-
-
-
-
-
-  
-  // function Lone_Star_School_of_Music () {
-  //   position = 'Piano/Music Production Instructor';
-  //   duties = ['2017 "Teacher of the Year"',
-  //             'work with students ages 5-adulthood, teaching various styles of piano and skills related to music production', 
-  //             'work with children who have special needs',
-  //             'have recieved additional training regarding students with special needs'] 
-  //   location = Austin, Tx;
-  //   return year(2014-Present)
-  // }
-
-
-  // function Delta_Arts_Alliance () {
-  //   position = 'Speaker';
-  //   duties = ['travel to low-income schools to speak about music theory, music production, music history, and performance', 
-  //             'work with regular and special needs students all of which learned to play bells and perform as a small ensemble'] 
-  //   location = Clarksdale, MS;
-  //   return year(2013)
-  // }
-
- 
-  // function No_Limits_For_Deaf_Children () {
-  //   position = 'Piano/Guitar Instructor';
-  //   duties = ['founded No Limits for Deaf Children music program',
-  //             'taught piano and guitar to students ranging in ages 5-21', 
-  //             'worked with students who are deaf or who are hard of hearing']
-  //   location = Culver_City, CA;
-  //   return year(2012-2013)
-  // }
-
-
-
-
-
-
-
-
-
-
-
-
-  // function Delta_State_University () {
-  //   field_of_study = 'Music'
-  //   return year(2006-2008)
-  // }
-
-  // function Full_Sail_University () {
-  //   field_of_study = 'Recording Arts'
-  //   graduated = true
-  //   return year(2009-2010)
-  // }
-
-  // function UT_Austin_Coding_Bootcamp () {
-  //   field_of_study = 'Full Stack Web Development'
-  //   graduated = true
-  //   return year(2018)
-  // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  // function Delta_State_University () {
-  //   field_of_study = 'Music'
-  //   return year(2006-2008)
-  // }
-
-
-  // function Full_Sail_University () {
-  //   graduated = true
-  //   return year(2009-2010)
-  // }
-
-
-  // function UT_Austin_Coding_Bootcamp () {
-  //   field_of_study = 'Full Stack Web Development'
-  //   return year(2018)
-  // }
